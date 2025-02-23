d4a832cbc75ee9e3546342dad84f5cf2eb955027
abrarbeg <140578092+abrarbeg@users.noreply.github.com>
2025-02-23 13:39:13 +0530

    Update post.js

--- post.js
+++ post.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
-const Post = require('./models/post'); // If the file is actually lowercase
-const PostSchema = new mongoose.Schema({
+
+const postSchema = new mongoose.Schema({
   title: String,
   category: String,
   content: String,
   image: String,
-  createdAt: { type: Date, default: Date.now },
+  createdAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model("Post", PostSchema);
+module.exports = mongoose.model("Post", postSchema);
